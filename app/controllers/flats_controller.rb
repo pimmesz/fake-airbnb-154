23d3165640831335383b7ff81bc93053f06cc068
Pim Zonneveld <pimzonneveld@gmail.com>
2018-06-12 23:50:27 +0200

    Finished flat CRUD

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,50 @@
+class FlatsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+
+  def index
+     @flats = policy_scope(Flat).order(created_at: :desc)
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+    authorize @flat
+  end
+
+  def new
+    @flat = Flat.new
+    authorize @flat
+  end
+
+  def create
+    @flat = Flat.create(flat_params)
+    @flat.user = current_user
+    authorize @flat
+    @flat.save!
+    redirect_to flats_path
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+    authorize @flat
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    authorize @flat
+    @flat.update_attributes(flat_params)
+    redirect_to flats_path
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    authorize @flat
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :flats
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable, :trackable
   devise :database_authenticatable, :registerable,
