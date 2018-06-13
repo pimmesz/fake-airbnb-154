e3b0ebbea33fb669de05d96d6a45c2e75f4eb81e
Pim Zonneveld <pimzonneveld@gmail.com>
2018-06-13 21:07:07 +0200

    Ajaxify search on homepage

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -50,6 +50,6 @@ class FlatsController < ApplicationController
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :city, :query)
+    params.require(:flat).permit(:name, :city)
   end
 end
